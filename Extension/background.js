2b2d56d602e5f65e75d41e774808679ee5e0cbfb
Aditya Pareek <123439444+Aditya4289@users.noreply.github.com>
2024-01-18 11:08:38 +0530

    Delete Extension directory

--- Extension/background.js
+++
@@ -1,8 +0,0 @@
-// background.js
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Fake Website Detector Extension Installed');
-});
-
-chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.tabId, { action: 'detectFakeWebsite' });
-});
